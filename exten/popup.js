03ec0169f40cab23b5e2d497451f158335953d9c
Nigel Alderton <nigel.alderton66@gmail.com>
2024-06-26 12:08:14 +0100

    tidy up exten/popup.js

--- exten/popup.js
+++ exten/popup.js
@@ -5,35 +5,55 @@ const statusUrl   = 'http://pi1b.lan/status/';
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', function() {
 
-  // fetch the YouTube videoId (if there is one) from the browser address bar
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-
-    let url_string = tabs[0].url;
-    var url = new URL(url_string);
-    var v = url.searchParams.get("v");
-
-    if (v)
-      { document.getElementById("videoId").value = v }
+  // parse the url for a videoId and if found, write it to the videoId textbox
+  parseUrl((videoId) => {
+    if (undefined !== videoId)
+      document.getElementById("videoId").value = videoId;
   });
-
-  // event listener to prevent forms redirect & refresh UI
-  document.querySelectorAll("form.refresh").forEach(function(element) {
-    element.addEventListener("submit", function(event){
-      event.preventDefault();
-      fetch(element.action, {
-        method: "post",
-        body: new URLSearchParams(new FormData(element))
-      })
-        .then(response => {
-          refreshUi()
-        })
+  
+  // event listener to prevent forms redirect and instead refresh the UI to redraw the popup
+  document.querySelectorAll("form.refresh").forEach((element) => {
+    element.addEventListener("submit", function(event) {
+      handleSubmit(element, event);
     })
   });
 
-  // fetch & show the queue size & autoplay status
+  // refresh the popup
   refreshUi();
     
 });
+
+
+// when a form submit button is clicked, prevent page redirect and instead refresh the UI to redraw the popup
+function handleSubmit(element, event) {
+    
+  event.preventDefault();
+  
+  fetch(element.action, {
+    method: "post",
+    body: new URLSearchParams(new FormData(element))
+  })
+    .then(response => {
+      refreshUi()
+    })
+}
+
+
+
+// parse the the browser address bar for a YouTube videoId
+function parseUrl(callback) {
+  
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  
+  chrome.tabs.query(queryOptions, tabs => {
+    
+    const url_string = tabs[0].url;
+    const url = new URL(url_string);
+    const videoId = url.searchParams.get("v");
+    
+    callback(videoId);
+  });
+}
 
 
 // refresh page
@@ -48,7 +68,7 @@ function refreshUi() {
       var size = json['size'].toString();
       document.getElementById("autoplay").innerHTML = json['autoplay'];   // display the autoplay status: on|off
       document.getElementById("size").innerHTML = size;                   // display the queue size
-      var badgeMsg = ('0' == size) ? '' : size;                               // queue size for the badge message
+      var badgeMsg = ('0' == size) ? '' : size;                           // queue size for the badge message
       setBadge(badgeMsg);
     })
 }
